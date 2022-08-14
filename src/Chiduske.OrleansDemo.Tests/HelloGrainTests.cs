71c842a95448ff045cbb6ffb0ce0307fe08cb64a
Christian Heiduschke <christian.heiduschke@immosparrow.ch>
2022-08-14 11:18:32 +0200

    add first unit test

---
+++ src/Chiduske.OrleansDemo.Tests/HelloGrainTests.cs
@@ -0,0 +1,24 @@
+﻿using Chiduske.OrleansDemo.GrainInterfaces;
+using FluentAssertions;
+using Orleans.TestingHost;
+using Xunit;
+
+namespace Chiduske.OrleansDemo.Tests;
+
+public class HelloGrainTests
+{
+    [Fact]
+    public async Task SaysHelloCorrectly()
+    {
+        var builder = new TestClusterBuilder();
+        var cluster = builder.Build();
+        await cluster.DeployAsync();
+
+        var hello = cluster.GrainFactory.GetGrain<IHello>(0);
+        var greeting = await hello.SayHello("Hi there!");
+        
+        await cluster.StopAllSilosAsync();
+
+        greeting.Should().Be("Hello, World");
+    }
+}
